cedba8d0e367e3fd21124b40ac368b7a9b8c25e4
enis <enis_akyol@yahoo.com>
2023-11-11 12:28:13 +0300

    ecommercelab with springdata including some errors

---
+++ src/main/java/com/cydeo/ECommerceLabApplication.java
@@ -0,0 +1,13 @@
+package com.cydeo.ecommercelab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ECommerceLabApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ECommerceLabApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/cydeo/ecommercelab/ECommerceLabApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cydeo.ecommercelab;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ECommerceLabApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
